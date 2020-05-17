77d2861c879081a994cb5b756367b635334a64d6
Max Simpson <maxsimpson95@gmail.com>
2020-05-17 16:11:27 +0800

    made and tested burger model for CRUD

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,67 @@
 const mongoClient = require("../config/mongo.js")
 
-const getConnection = async () => {
-    return new Promise((resolve, reject) => {
-        mongoClient.connect((err, client) => {
-            if (err) { 
-                console.log('failed to connect') 
-                reject(err)
-            } else {
-                console.log('connected')
-                resolve(client.db('eat_da_burger').collection('burgers'))
-            }
-        });
-    })
+class Burger {
+    async getConnection() {
+        return new Promise((resolve, reject) => {
+            mongoClient.connect((err, client) => {
+                if (err) {
+                    console.log('failed to connect')
+                    reject(err)
+                } else {
+                    console.log('connected')
+                    resolve(client.db('eat_da_burger').collection('burgers'))
+                }
+            });
+        })
+    }
+    async insertOne(burger) {
+        //return a promise to insert the burger. successful returns the burger else returns the error
+        return new Promise((resolve, reject) => {
+            this.getConnection()
+                .then((burgers) => {
+                    burgers.insertOne(burger, (err, result) => {
+                        if (err) reject(err);
+                        resolve(result.ops[0])
+                    })
+                })
+                .catch((error) => { throw error })
+        })
+    }
+    async selectAll() {
+        //return a promise to insert the burger. successful returns the burger else returns the error
+        return new Promise((resolve, reject) => {
+            this.getConnection()
+                .then(async (burgers) => {
+                    const cursor = burgers.find()
+                    let results = await cursor.toArray()
+                    // if (err) reject(err);
+                    resolve(results)
+
+                })
+                .catch((error) => { throw error })
+        })
+    }
+    async updateOne(id,burger) {
+        //return a promise to update a burger. successful returns the burger else returns the error
+        //if burger contains _id it must match the id param
+        //https://docs.mongodb.com/manual/tutorial/update-documents/
+
+        return new Promise((resolve, reject) => {
+            this.getConnection()
+                .then((burgers) => {
+                    burgers.replaceOne(
+                        { _id: id },
+                        burger
+                    )
+                        .then((result) => {
+
+                            resolve(result.ops[0])
+                        })
+                        .catch((error) => { reject(error) });;
+                })
+                .catch((error) => { throw error })
+        })
+    }
 }
 
-getConnection()
-    .then((burgers) => {
-        burgers.insertOne({
-            burger_name: 'cheeseburger',
-            devoured: 0
-        }, (err, result) => {
-            if (err) console.log(err);
-            else console.log(result.ops[0])
-        })
-    })
-    .catch((error) => { throw error })
-
-
+module.exports = Burger
