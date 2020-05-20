65b66e5dba0e8a728c65802ce8d3dc237d2dc9e8
Max Simpson <maxsimpson95@gmail.com>
2020-05-20 22:32:23 +0800

    removed user and password for mongo connection

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
-const username = process.env.MONGO_USER || "max"
-const password = process.env.MONGO_PASS || "fIh0SsjisQvfhMBe"
+const username = process.env.MONGO_USER //|| "max"
+const password = process.env.MONGO_PASS //|| "fIh0SsjisQvfhMBe"
 // heroku config:set MONGO_PASS="fIh0SsjisQvfhMBe"
 // heroku config:set MONGO_USER="max"
 const uri = `mongodb+srv://max:${password}@cluster0-fkwlp.mongodb.net/test?retryWrites=true&w=majority`;
